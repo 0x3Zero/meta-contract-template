5520189e6e53fab770b2af72290fc2dc7567249a
iqbalbaharum <muhammad.iqbal.baharum@gmail.com>
2023-03-31 17:33:38 +0800

    add collabeat sample

---
+++ src/main.rs
@@ -0,0 +1,73 @@
+#![allow(improper_ctypes)]
+
+mod types;
+
+use marine_rs_sdk::marine;
+use marine_rs_sdk::module_manifest;
+use marine_rs_sdk::WasmLoggerBuilder;
+use types::MetaContract;
+use types::Metadata;
+use types::Transaction;
+use types::{FinalMetadata, MetaContractResult};
+
+module_manifest!();
+
+pub fn main() {
+    WasmLoggerBuilder::new()
+        .with_log_level(log::LevelFilter::Info)
+        .build()
+        .unwrap();
+}
+
+#[marine]
+pub fn on_execute(
+    contract: MetaContract,
+    metadatas: Vec<Metadata>,
+    transaction: Transaction,
+) -> MetaContractResult {
+    let mut finals: Vec<FinalMetadata> = vec![];
+
+    // Only 10 beat
+    if metadatas.len() > 13 {
+        return MetaContractResult {
+            result: false,
+            metadatas: finals,
+            error_string: "Can not be more than 10 beats".to_string(),
+        };
+    }
+
+    if metadatas.len() <= 0 {
+        finals.push(FinalMetadata {
+            public_key: contract.public_key.clone(),
+            alias: "name".to_string(),
+            content: format!("Collabeat #{}", transaction.token_id),
+        });
+
+        finals.push(FinalMetadata {
+            public_key: contract.public_key.clone(),
+            alias: "description".to_string(),
+            content: "Co-Create, Collaborate and Own The Beat".to_string(),
+        });
+
+        finals.push(FinalMetadata {
+            public_key: contract.public_key.clone(),
+            alias: "image".to_string(),
+            content: "ipfs://".to_string(),
+        });
+    }
+
+    finals.push(FinalMetadata {
+        public_key: transaction.public_key,
+        alias: transaction.alias,
+        content: transaction.data,
+    });
+
+    MetaContractResult {
+        result: true,
+        metadatas: finals,
+        error_string: "".to_string(),
+    }
+}
+
+// Service
+// - curl
---
+++ src/types.rs
@@ -0,0 +1,53 @@
+use marine_rs_sdk::marine;
+
+#[marine]
+pub struct MetaContractResult {
+    pub result: bool,
+    pub metadatas: Vec<FinalMetadata>,
+    pub error_string: String,
+}
+
+#[marine]
+pub struct FinalMetadata {
+    pub public_key: String,
+    pub alias: String,
+    pub content: String,
+}
+
+#[marine]
+#[derive(Debug)]
+pub struct Metadata {
+    pub data_key: String,
+    pub alias: String,
+    pub cid: String,
+    pub public_key: String,
+    pub enc: String,
+}
+
+#[marine]
+#[derive(Debug)]
+pub struct Transaction {
+    pub hash: String,
+    pub token_key: String,
+    pub data_key: String,
+    pub nonce: i64,
+    pub from_peer_id: String,
+    pub host_id: String,
+    pub status: i64,
+    pub data: String,
+    pub public_key: String,
+    pub alias: String,
+    pub timestamp: u64,
+    pub meta_contract_id: String,
+    pub method: String,
+    pub error_text: String,
+    pub token_id: String,
+}
+
+#[marine]
+#[derive(Debug, Default, Clone)]
+pub struct MetaContract {
+    pub token_key: String,
+    pub meta_contract_id: String,
+    pub public_key: String,
+}
